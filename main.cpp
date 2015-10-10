f48be07fc1cec08cfc24d55ce58681b0fbf49ee8
StelthDestroyeR <tvink.kos@rambler.ru>
2015-10-10 11:48:59 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication6.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "header.h"
+using namespace std;
+node *stack = NULL;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	node *top = new node[10];
+	delete[] top;
+	system("pause");
+	return 0;
+}
+
